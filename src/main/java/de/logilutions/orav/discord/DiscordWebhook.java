2b3bb10e7ae022527dd7ffedbc4d8c5f8703c01f
konrad2002 <konrad@schwimmteamerzgebirge.de>
2021-11-27 14:49:40 +0100

    discord debug print

--- src/main/java/de/logilutions/orav/discord/DiscordWebhook.java
+++ src/main/java/de/logilutions/orav/discord/DiscordWebhook.java
@@ -146,6 +146,7 @@ public class DiscordWebhook {
         connection.setRequestMethod("POST");
 
         OutputStream stream = connection.getOutputStream();
+        System.out.println(json.toString());
         stream.write(json.toString().getBytes());
         stream.flush();
         stream.close();
